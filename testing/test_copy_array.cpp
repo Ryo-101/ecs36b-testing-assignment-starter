d5ef06ef1df71204adc8567cf64443a1d1171f75
Braulio Macedo <brodrigomv@gmail.com>
2026-04-11 19:17:12 -0700

    RC_TEST_PROP CopyArraysTest PropertyCopyWasMade Code In test_copy_array.cpp Has Been Implemented (Syntax Errors Present)

--- testing/test_copy_array.cpp
+++ testing/test_copy_array.cpp
@@ -143,6 +143,21 @@ RC_GTEST_PROP(CopyArrayTests,
   * Don't forget to free any memory that was dynamically allocated as part of your test.
   */
 
+    values = rc::gen::suchThat(
+                                rc::gen::arbitrary<std::vector<int>>(),
+                                [](const auto vector)
+                                {
+                                    return vector.size() > 1;
+                                }
+                                ).as("vectorInt");
+
+    int* copy = copy_array(values, values.size());
+
+    for (int i = 0; i < values.size(); i++)
+    {
+        RC_ASSERT((values + i) != (copy + i));
+    }
+
 }
 
 
