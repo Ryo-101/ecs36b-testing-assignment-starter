42ebe479206094cc6d104b611d8810bb3a8c4b6b
Braulio Macedo <brodrigomv@gmail.com>
2026-04-13 13:43:49 -0700

    RC_TEST_PROP SwapTests PropertySwapTwoValues And RC_TEST_PROP SwapTests PropertySwapValuesInArray Code In test_swap.cpp Has Been Fixed (Thank you Professor Butner!)

--- testing/test_swap.cpp
+++ testing/test_swap.cpp
@@ -47,11 +47,17 @@ RC_GTEST_PROP(SwapTests,
      * Swap two values and see if the swap was successful.
      */
 
-    a_start = rc::gen::arbitrary<int>();
-    b_start = rc::gen::arbitrary<int>();
+    // Rule of Thumb:
+    // If I need to constrain the generated values to specifics, must define generators with some function(s)
+    // If I don't care about the specifics, just put parameters in the parenthesis at the top
 
-    int tempA = a_start;
-    int tempB = b_start;
+    // Does the same as the parameters at the top, so can be deleted
+    // Note: with every generator, it must be unreferenced to assign it to a variable
+    a_start = *rc::gen::arbitrary<int>();
+    b_start = *rc::gen::arbitrary<int>();
+
+    const int tempA = a_start;
+    const int tempB = b_start;
 
     swap(&a_start, &b_start);
 
@@ -63,25 +69,56 @@ RC_GTEST_PROP(SwapTests,
 
 RC_GTEST_PROP(SwapTests,
               PropertySwapValuesInArray,
-              (const std::vector<int>& values)
+              ()
 ) {
     /*
      * Swap two values in an array. See that they swapped and the others did not
      */
 
-    values = rc::gen::suchThat(
+    // Generates a vector with the following constraints:
+        // A arbitrary vector made of int values
+        // Vector is of length greater than 1 (in order to swap 2 elements)
+        // Refer to such vector as "vectorInt" in test logs
+    auto values = *rc::gen::suchThat(
                                 rc::gen::arbitrary<std::vector<int>>(), // generator
                                 [](const auto &vector) // lambda function
                                 {
                                     return vector.size() > 1; // min length of vectors is 2
                                 }).as("vectorInt"); // refer to a vectorInt in test logs
 
-    int tempOne = values[0];
-    int tempTwo = values[1];
+    // Creates a copy of vector values and refer it as original
+    const auto original(values);
 
-    swap(values, values + 1);
+    // Generates an int value with the following constraints:
+        // An int value that is between 0 and the size of vector - 1
+    const size_t index1 = *rc::gen::inRange<size_t>(0, values.size() - 1).as("index1");
+    // Generates an int value with the following constraints:
+        // An int value that is between 0 and size of vector - 1 BUT
+        // Cannot be the same as the int value of index 1
+    const size_t index2 = *rc::gen::distinctFrom(
+        rc::gen::inRange<size_t>(0, values.size() - 1),
+        index1
+        );
 
-    RC_ASSERT(values[0] == tempTwo);
-    RC_ASSERT(values[1] == tempOne);
+    // Save the values to check later with RC_ASSERT
+    const int tempOne = values[index1];
+    const int tempTwo = values[index2];
+
+    // Call the function being tested
+    swap(values.data() + index1, values.data() + index2);
+
+    // Check whether the values were actually swapped with RC_ASSERT
+    RC_ASSERT(values[index1] == tempTwo);
+    RC_ASSERT(values[index2] == tempOne);
+
+    // Additionally, check if other int values haven't been changed
+    for (size_t i = 0; i < original.size(); i++)
+    {
+        if (i != index1 && i != index2)
+        {
+            RC_ASSERT(original[i] == values[i]);
+        }
+    }
+
 
 }
